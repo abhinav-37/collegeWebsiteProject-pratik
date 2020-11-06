488c83fe9f16461c93be5897d1edf7e2c6c2957e
abhinav sharma <abhiut37@gmail.com>
2020-11-06 20:34:02 +0530

    fixed linking bugs

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const passportLocalMongoose = require("passport-local-mongoose");
 const express = require("express");
 const app = express();
 app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
 app.use(
     bodyParser.urlencoded({
         extended: true,
